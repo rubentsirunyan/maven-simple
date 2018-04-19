51fe315ee04b339c4390dc7729dbc87327490b11
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2018-04-19 13:04:18 +0400

    Adding befores

--- src/test/java/com/github/jitpack/AppTest.java
+++ src/test/java/com/github/jitpack/AppTest.java
@@ -1,23 +1,36 @@
 package com.github.jitpack;
 
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class AppTest 
-{
+public class AppTest {
+
     App app = new App();
 
+    @BeforeClass
+    public static void onceExecutedBeforeAll() {
+
+        System.out.println("@BeforeClass: onceExecutedBeforeAll");
+
+    }
+
+    @Before
+    public void executedBeforeEach() {
+
+        System.out.println("@Before: executedBeforeEach");
+
+    }
+
     @Test
-    public void testApp()
-    {
+    public void testApp() {
+
         assertEquals("Hello world", app.greet("world"));
     }
 
     @Test
-    public void testTrue()
-    {
-        assertTrue( app.truer("val") );
+    public void testTrue() {
+        assertTrue(app.truer("asd"));
     }
 }
